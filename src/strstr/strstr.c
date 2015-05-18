9a4bcb3aa4d90526fc5ae27e80cc32ab3d159dd6
anzhongqi <anzhongqi@gmail.com>
2015-05-18 21:06:30 +0800

    tiny change to strstr

--- src/strstr/strstr.c
+++ src/strstr/strstr.c
@@ -1,5 +1,5 @@
 /*
- * this is naive ... maybe buggy :)
+ * this is simple and naive ... and maybe buggy :)
  * */
 
 #include <stdlib.h>
@@ -12,17 +12,17 @@ char* strstr(const char *haystack, const char *needle)
 	const char* hstk;
 	const char* ndl;
 
-	if (!*needle) return (char*) haystack;
+	if (*needle == 0) return (char*) haystack;
 
-	while (*haystack != 0) {
+	while (*haystack) {
 		if (*haystack == *needle) {
 			hstk = haystack + 1;
 			ndl  = needle   + 1;
-			while (*hstk != 0 && *ndl != 0)
+			while (*hstk && *ndl)
 				if (*hstk == *ndl) hstk++, ndl++;
 				else break;
-			if (!*ndl) return (char*) haystack;
-			else if (!*hstk) return NULL;
+			if (*ndl == 0) return (char*) haystack;
+			if (*hstk == 0) return NULL;
 		}
 		haystack++;
 	}
@@ -34,7 +34,8 @@ int main()
 {
 	char a[MAX], b[MAX];
 	char* result;
-	void call_strstr() { result = (char*) strstr(a, b); }    // i know this is wired ... and that use of EOF may be wrong ...
+
+	void call_strstr() { result = strstr(a, b); }            // i know this is wired ... and that use of EOF may be wrong ...
 	scanf("%s%s", a, b) == EOF ? exit(1) : call_strstr();    // i just want to shut that annoying warn_unused_result warning ...
 	                                                         // i know i'm stupid ... :)
 	if (result) printf("%ld\n", result-a+1);
