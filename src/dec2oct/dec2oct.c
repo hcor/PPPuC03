28490eaad6d71478f4711d9fefbc93d8621f5627
anzhongqi <anzhongqi@gmail.com>
2015-05-28 15:40:53 +0800

    change `num` to `dec`.

--- src/dec2oct/dec2oct.c
+++ src/dec2oct/dec2oct.c
@@ -4,14 +4,14 @@
 
 int main()
 {
-	int num, i;
+	int dec, i;
 	char oct[LEN+1] = { 0 };    // if an array is partially initialized, elements that are not initialized 
 	                            // receive the value 0 of the appropriate type.
-	if (scanf("%d", &num) == 1)
-		if (0 <= num && num <= 32767) {
+	if (scanf("%d", &dec) == 1)
+		if (0 <= dec && dec <= 32767) {
 			for (i = LEN; i > 0; i--) {
-				oct[i-1] = num % 8 + '0';
-				num /= 8;
+				oct[i-1] = dec % 8 + '0';
+				dec /= 8;
 			}
 			printf("%s\n", oct);
 		}
